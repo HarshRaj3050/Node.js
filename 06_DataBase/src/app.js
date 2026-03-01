67cb55b3e579b80be4ace356c45c97de55a02fb4
HarshRaj3050 <harshraj3060@gmail.com>
2026-03-01 11:38:36 +0530

    Project 1 complete backend

--- 06_DataBase/src/app.js
+++ 06_DataBase/src/app.js
@@ -4,6 +4,7 @@ const noteModel = require('./models/note.model');
 const app = express();
 app.use(express.json());
 
+
 app.post('/notes', async (req, res) => {
     const data = req.body;
     await noteModel.create({
@@ -16,6 +17,44 @@ app.post('/notes', async (req, res) => {
     })
 })
 
+
+app.get('/notes', async (req, res) => {
+    const note = await noteModel.find({       // return data in the Array format []
+        // age: 20
+    }); 
+
+    // find => [{},{}] or []
+    // findOne => {} or null
+
+    res.status(200).json({
+        message: "Notes feach successfully!",
+        notes: note
+    })
+})
+
+
+app.delete('/notes/:id', async (req, res) => {
+    const id = req.params.id
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+
+    res.status(200).json({
+        message: "Note delete successfully"
+    })
+})
+
+
+app.patch('/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    const newAge = req.body.age
+
+    await noteModel.findOneAndUpdate({_id: id}, {age: newAge})
+
+    res.status(200).json({
+        message: "new Age updated successfully"
+    })
+})
 
 
 module.exports = app;
---
+++ Project_01/backend/src/app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const express = require('express');
+const multer = require('multer');
+const uploadFile = require('./services/storage.service');
+const postModel = require('./models/post.model');
+
+const app = express();
+
+const upload = multer({storage: multer.memoryStorage()})
+
+app.use(express.json());
+
+app.post('/create-post',upload.single("image") ,async (req, res) => {
+
+    const result = await uploadFile(req.file.buffer);
+
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+
+    return res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
+})
+
+
+app.get('/posts', async (req, res) => {
+    const posts = await postModel.find()
+
+    res.status(200).json({
+        message: "posts fetched successfully",
+        posts
+    })
+})
+
+
+
+
+
+module.exports = app;
